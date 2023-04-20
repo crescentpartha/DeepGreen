00f9b3cd83d5ba0274bafab6570e3e0b8cc243aa
crescentpartha <crescentpartha406@gmail.com>
2023-04-20 13:47:14 +0600

    Setup routes and react-router

---
+++ deepgreen/src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
