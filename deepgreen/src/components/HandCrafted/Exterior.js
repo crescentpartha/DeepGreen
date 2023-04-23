43f308875354cd2583b9506e2b60891a2ba3cbcb
crescentpartha <crescentpartha406@gmail.com>
2023-04-23 23:56:49 +0600

    Replaced block image by background image inside the handcrafted section

--- deepgreen/src/components/HandCrafted/Exterior.js
+++ deepgreen/src/components/HandCrafted/Exterior.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import chair from './../../images/handcrafted/chair.png';
+import chair from './../../images/handcrafted/chair-flip.png';
 import { Link } from 'react-router-dom';
 
 const Exterior = () => {
     return (
-        <div className='grid grid-cols-2 gap-4 justify-center items-center bg-info pl-4 sm:pl-10 py-4 sm:py-10 w-96 sm:h-80 max-h-full'>
-            <div className='text-start'>
+        <div className='grid grid-cols-2 gap-4 justify-center items-center md:items-start bg-info pl-4 sm:pl-10 pt-10 w-96 sm:h-80 max-h-full'>
+            <div className='text-start pb-10'>
                 <h4 className='uppercase font-coresens text-xs sm:text-sm'>From <span className='text-primary'>$125.00</span></h4>
                 <h2 className='uppercase font-coresens text-xl sm:text-2xl my-2'>Handcrafted Exterior</h2>
                 <p className='font-kiro text-xs mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi debitis libero perferendis odit possimus deserunt?</p>
                 <Link to='/' className='uppercase font-coresens text-xs sm:text-sm mt-2'>Explore</Link>
                 <hr className='w-10 border-t-2 border-primary' />
             </div>
-            <div className='mx-auto m-0'>
+
+            {/* Block Image Section */}
+            {/* <div className='mx-auto m-0'>
                 <img className='max-w-full h-48' src={chair} alt="Chair" />
+            </div> */}
+
+            {/* Background Image Section */}
+            <div className="hero h-full bg-no-repeat bg-left-bottom bg-cover" style={{ backgroundImage: `url(${chair})` }}>
+                <div className="hero-overlay bg-opacity-10"></div>
+                <div className="hero-content text-center text-neutral-content w-full p-0 pt-20 pb-5">
+                    {/* There were no content over image */}
+                </div>
             </div>
         </div>
     );
--- deepgreen/src/components/HandCrafted/Interior.js
+++ deepgreen/src/components/HandCrafted/Interior.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Interior = () => {
     return (
-        <div className='flex flex-col gap-4 justify-center items-center bg-neutral px-4 sm:px-10 pt-4 sm:pt-10 w-96 sm:h-80 max-h-full'>
+        <div className='flex flex-col gap-4 justify-center items-center bg-neutral px-4 sm:px-10 pt-10 w-96 sm:h-80 max-h-full'>
             <div className='text-center'>
                 <h4 className='uppercase font-coresens text-xs sm:text-sm'>From <span className='text-accent'>$125.00</span></h4>
                 <h2 className='uppercase font-coresens text-xl sm:text-2xl my-2'>Handcrafted Interior</h2>
@@ -12,8 +12,18 @@ const Interior = () => {
                 <Link to='/' className='uppercase font-coresens text-xs sm:text-sm mt-2'>Explore</Link>
                 <hr className='w-10 mx-auto border-t-2 border-accent' />
             </div>
-            <div className='mx-auto m-0'>
+
+            {/* Block Image Section */}
+            {/* <div className='mx-auto m-0'>
                 <img className='max-w-full h-32' src={table} alt="Table" />
+            </div> */}
+
+            {/* Background Image Section */}
+            <div className="hero h-full bg-no-repeat bg-top bg-cover" style={{ backgroundImage: `url(${table})` }}>
+                <div className="hero-overlay bg-opacity-10"></div>
+                <div className="hero-content text-center text-neutral-content w-full p-0 pt-20 pb-5">
+                    {/* There were no content over image */}
+                </div>
             </div>
         </div>
     );
